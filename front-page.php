00b4d59170344a34e6a48c6e882fd6886d9c2566
Constantino Schillebeeckx <titous@gmail.com>
2016-07-11 18:19:39 -0700

    initial commit

---
+++ front-page.php
@@ -0,0 +1,23 @@
+<?php get_header(); ?>
+
+    <main role="main">
+    <!-- section -->
+    <section>
+
+
+    <!-- article -->
+    <article id="search">
+
+        <?php // content here ?>
+
+    </article>
+    <!-- /article -->
+
+    </section>
+    <!-- /section -->
+    </main>
+
+<?php //get_sidebar(); ?>
+
+<?php get_footer(); ?>
+
